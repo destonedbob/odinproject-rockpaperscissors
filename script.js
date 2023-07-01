0b20730c08c32cfbd3ee86f53b10a82514bdc4f4
Deston Chung <destonchung@gmail.com>
2023-07-01 11:29:22 +0800

    Add function to title case a word

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function getComputerChoice() {
     let choiceIndex = Math.floor(Math.random() * (3-0));
     return choices[choiceIndex];
 }
+
+
+function titleCase(string) {
+    return string[0].toUpperCase().concat(string.slice(1).toLowerCase())
+}
