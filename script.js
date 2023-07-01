f8aeed8de9933c81f2b7724a26969e7a05b78b90
Deston Chung <destonchung@gmail.com>
2023-07-01 11:30:01 +0800

    Add function to play a single round of rock paper scissors

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ function getComputerChoice() {
 function titleCase(string) {
     return string[0].toUpperCase().concat(string.slice(1).toLowerCase())
 }
+
+function playRound(playerSelection, computerSelection) {
+    let playerSelectionCleaned = playerSelection.toLowerCase();
+    let computerSelectionCleaned = computerSelection.toLowerCase();
+
+    if (playerSelectionCleaned === computerSelectionCleaned) {
+        return "Draw!"
+    } else if ((playerSelectionCleaned == 'rock' && computerSelection == 'scissors') ||
+               (playerSelectionCleaned == 'paper' && computerSelection == 'rock') ||
+               (playerSelectionCleaned == 'scissors' && computerSelection == 'paper')
+    ) {
+        return `You Win! ${titleCase(playerSelectionCleaned)} beats ${titleCase(computerSelection)}`
+    } else {
+        return `You Lose! ${titleCase(computerSelection)} beats ${titleCase(playerSelectionCleaned)}`
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection));
