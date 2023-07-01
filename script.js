3a87c4951b25220e2e8352768b16cd387252116b
Deston Chung <destonchung@gmail.com>
2023-07-01 11:53:30 +0800

    Add functionality to prompt user for Valid move

--- script.js
+++ script.js
@@ -19,13 +19,36 @@ function playRound(playerSelection, computerSelection) {
                (playerSelectionCleaned == 'paper' && computerSelection == 'rock') ||
                (playerSelectionCleaned == 'scissors' && computerSelection == 'paper')
     ) {
-        return `You Win! ${titleCase(playerSelectionCleaned)} beats ${titleCase(computerSelection)}`
+        return [`You Win the Round! ${titleCase(playerSelectionCleaned)} beats ${titleCase(computerSelection)}`, True]
     } else {
-        return `You Lose! ${titleCase(computerSelection)} beats ${titleCase(playerSelectionCleaned)}`
+        return [`You Lose the Round! ${titleCase(computerSelection)} beats ${titleCase(playerSelectionCleaned)}`, False]
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection));
+// Play game 5 times
+let playerScore = 0;
+let CompScore = 0;
+for (let i = 0; i < 1; i++) {
+    // Keep score
+    // Prompt users
+    let currPlayerMove = prompt('Select a move by typing "rock", "paper", or "scissors"').toLowerCase();
+    while (!['rock', 'paper', 'scissors'].includes(currPlayerMove)) {
+        currPlayerMove = prompt('Invalid Move! Select a move by typing "rock", "paper", or "scissors"').toLowerCase();
+    }
+
+    console.log(`End of Round ${i+1}`)
+    // Initialize player and computer score to 0
+    // Loop through 5 times
+    // In each time, prompt user for a move (while move is not valid, repeat till it is)
+    // get computer's move
+    // play round
+    // add 1 to winner's score
+    // Declare winner
+}
+
+
+
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(computerSelection)
+// console.log(playRound(playerSelection, computerSelection));
